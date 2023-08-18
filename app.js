3fcb98ee4ac503568321ac9e498ce15892021294
Iryna Tsupryk <iranaumovich0@gmail.com>
2023-08-18 14:12:18 +0200

    fix: добавить id юзера

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } =
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } =
   process.env;
 
 const app = express();
